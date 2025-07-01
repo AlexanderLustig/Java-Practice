15a62611293d6257605e0b986eb42c771de81f76
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-01 22:13:19 +0000

    commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args){
+        
+        //This is my first java program
+
+        /*type "sout" then tab types System.out.println */
+
+        System.out.println("I like pizza\n");
+        System.out.println("It''s really good");
+        
+    }
+}
