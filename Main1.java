3c5082472b4dee17b82a26fe3c03dc7552393d11
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-02 15:05:53 -0400

    Update and rename Day 1.java to Main1.java

--- Main1.java
+++ Main1.java
@@ -1,9 +1,7 @@
-public class Main {
-
+public class Main1 {
+    //day 1(7/1/2025)
     public static void main(String[] args){
         
-        //This is my first java program
-
         /*type "sout" then tab types System.out.println */
 
         System.out.println("I like pizza\n");
