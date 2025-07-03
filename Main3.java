f3dbd6a3db079726af8fe3d2d3b3144ed5d6dbfe
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-03 15:03:16 -0400

    Update Main3.java

--- Main3.java
+++ Main3.java
@@ -1,8 +1,23 @@
-public class Main1 {
+import java.util.Scanner;
+
+public class Main3{
     //day 3(7/3/2025)
     public static void main(String[] args){
         
-       
-        
+       Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Your name is " + name);
+
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        System.out.println("Your age is " + age);
+
+        System.out.println("What is your gpa");
+        double gpa = scanner.nextDouble();
+        System.out.println("Your gpa is " + gpa);
+
+        scanner.close(); 
     }
 }
