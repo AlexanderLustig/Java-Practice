6b8fcc437dd0fa57cc62e657578d6696be1d1889
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-05 15:01:08 -0400

    Create Main5.java

---
+++ Main5.java
@@ -0,0 +1,27 @@
+public class Main5{
+    //day 5(7/5/2025)
+
+    public static void main(String[] args){
+
+    int x = 10;
+    x++;
+    int y = 2;
+    int z;
+
+    z = x + y;
+    System.out.println(z);
+
+    z -= 8;
+    System.out.println(z);
+
+    z *= 4;
+    System.out.println(z);
+
+    z /= y;
+    System.out.println(z);
+
+    z %= 2; //for even or odd
+    System.out.println(z);
+        
+    }
+}
