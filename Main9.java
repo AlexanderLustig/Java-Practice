7f8a6d1bee24a6d5f603dbaa09b83890eb58e598
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-18 12:12:18 -0400

    Create Main9.java

---
+++ Main9.java
@@ -0,0 +1,29 @@
+//day 9(7/18/2025) -- Had a second date with Jenan. Went great and we arranged 2 new dates next week. Also her birthday next week so imma get some gifts ig. 
+//And now Sierra asking me if I still like her like ughhh. 
+
+public class Main9 {
+    public static void main(String[] args) {
+       
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+
+        double result = Math.pow(2, 3);
+        System.out.println(result);
+        result = Math.abs(-5);
+        System.out.println(result);
+        result = Math.sqrt(9);
+        System.out.println(result);
+        result = Math.round(3.14159);
+        System.out.println(result);
+        result = Math.ceil(3.14159);
+        System.out.println(result);
+        result = Math.floor(3.14159);
+        System.out.println(result);
+        result = Math.max(10,20);
+        System.out.println(result);
+        result = Math.min(10,20);
+        System.out.println(result);
+     
+    }
+}
+
