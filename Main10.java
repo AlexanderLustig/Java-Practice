5064317037df161fb4604e485ed928387eb57fb7
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-19 13:02:42 -0400

    Create Main10.java

---
+++ Main10.java
@@ -0,0 +1,21 @@
+//day 10(7/19/2025) -- OK so if finally was able to cut off Sierra. Bro she tried to blame me at first, but when i sent the msg ending it she basically begged that she 
+//would change and we could make it work yada yada like damn girl chill u were insulting me the whole time and didn't even wanna go out like why u sad. But ya besides that
+// rn iI think things are going well with Jenan, but like idk we dont text that much and there isnt any easy flowing convos over text usually which scares me cause like idk
+//how compatable we are cause of that. Like I still wanna keep talking to her but idk how it will play out in the future
+
+public class Main10 {
+    public static void main(String[] args) {
+    
+    String name = "Spongebob";
+    char firstLetter = 'S';
+    int age = 30;
+    double height = 60.5;
+    boolean isEmployed = true;
+
+    System.out.printf("Hello %s\n", name);
+    System.out.printf("Your name starts with a %c\n", firstLetter);
+     
+    }
+}
+
+
