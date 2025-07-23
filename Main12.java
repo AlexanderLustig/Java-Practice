03887de734fb14460705d9e8a74b769becaee03e
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-23 13:04:13 -0400

    Create Main12.java

---
+++ Main12.java
@@ -0,0 +1,23 @@
+// day 12 (7/23/2025) -- Had a 3rd date with Jenan. Went SUPPERRR amazing. During the movie she was leaning on my shoulder for most of it and like
+//whenever we talked we got really clsoe to eachother and idk that felt nice. Plus we also held hands that day also. But my original plan to hold her hand was
+//ruined cause a dumbass tree delayed the train. But it still worked out in the end. Now i wanna try to kiss her on the 4th date. Plus i need to give her her
+//presents so we will see how that goes next.
+
+public class Main12 {
+    public static void main(String[] args) {
+       
+    boolean isStudent = true;
+    int age = 17;
+    if(isStudent){
+        if(age >= 18){
+            System.out.println("Price of admissions is $15");
+        }else{
+            System.out.println("Price of admissions is $0");
+        }
+    }else if(age >= 18){
+            System.out.println("Price of admissions is $25");
+        }else{
+            System.out.println("Price of admissions is $10");
+        }
+    }
+}
