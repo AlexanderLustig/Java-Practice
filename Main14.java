30918657437c9776bd5058b490c3dee3b7d2be99
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-26 16:02:36 -0400

    Create Main14.java

---
+++ Main14.java
@@ -0,0 +1,14 @@
+// day 14 (7/26/2025) -- Ok so me and Jenan just played a game for a bit. Mid game but it was fun cause it was with her. And now im thinking that I should get Xbox Ultimate
+// that would allow us to play a bunch of games together that are coop. Plus I'm gonna need it anyways for Helldivers 2 so like ya.
+
+public class Main14 {
+    public static void main(String[] args) {
+
+        String email = "alexanderlustig77@gmail.com";
+        String username = email.substring(0, email.indexOf("@"));
+        String domain = email.substring(email.indexOf("@"));
+
+        System.out.println("Your username is: " + username);
+                System.out.println("Your domain is: " + domain);
+    }
+}
