924fd61c35f3926dfa950792fb065512c214238a
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-29 12:03:06 -0400

    Create Main15.java
    Conversion project

---
+++ Main15.java
@@ -0,0 +1,31 @@
+// day 15 (7/29/2025) -- Had a date yesterday. I went super good. We also kissed which was like my entire goal for the date. She even kissed me goodbye which I really liked
+// plus she said she liked her gifts. So overall a win.
+
+// day 15 (7/29/2025) -- 
+
+import java.util.Scanner;
+
+public class Main15 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Do you want to convert lbs to kgs (1) or kgs to lbs (2)? Input 1 or 2: ");
+    int option = scanner.nextInt();
+    
+    double conversion = 0;
+
+    if(option == 1){
+        System.out.print("How many lbs do you want to convert?: ");
+        double lbs = scanner.nextDouble();
+        conversion = lbs / 2.20462;
+        System.out.println(lbs + "lbs converges to " + conversion + "kgs");
+    }else{
+        System.out.print("How many kgs do you want to convert?: ");
+        double kgs = scanner.nextDouble();
+        conversion = kgs * 2.20462;
+        System.out.println(kgs + "kgs converges to " + conversion + "lbs");
+    }
+
+    scanner.close();
+    }
+}
