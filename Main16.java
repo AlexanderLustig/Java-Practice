44315be06fdb170f9e1331ff298ac3db78cfc2b8
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-07-30 12:01:07 -0400

    Update Main16.java

--- Main16.java
+++ Main16.java
@@ -1,9 +1,16 @@
-// day 16 (7/30/2025) -- 
+// day 16 (7/30/2025) -- Nothing too interesting today. The door to the bathroom finally was installed so that marks the end
+// to all the construction to the apt; atleast rn until mom decides to find something new
 
-import java.util.Scanner;
-
-public class Main15 {
+public class Main16 {
     public static void main(String[] args) {
     
+    int score = 70;
+    String passOrPFail = (score >=60) ? "PASS" : "FAIL";
+    System.out.println(passOrPFail);
+
+    int number = 3;
+    String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
+    System.out.println(evenOrOdd);
+
     }
 }
