24d1d585b1719aaa9a8e3b4e2156de22bd06fd9d
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-01 12:41:03 -0400

    Create Main17.java

---
+++ Main17.java
@@ -0,0 +1,27 @@
+// day 17 (8/01/2025) -- OK, so I have a date with Jenan tmrw which im exictied fr, but for some reason she is being very distant
+// recently. IDK why like texting less then usual. And its werid cause the night before things were going well, but like I went to 
+// work and ya we agreed to go out on Saturday, but like then nothing for the rest of the day. 0 texting. idk atp. 
+
+// day 17 (8/01/2025) -- 
+
+import java.util.Scanner;
+
+public class Main17 {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Do you want to convert F to C (1) or C to F (2)? Input 1 or 2: ");
+    int option = scanner.nextInt();
+    
+    System.out.println("What is the temprature in that degrees?: ");
+    double temprature = scanner.nextDouble();
+
+    double finalTemp = 0;
+
+    finalTemp = (option == 1) ? ((temprature - 32) / 1.8) : ((temprature * 1.8) + 32);
+
+    System.out.printf("Converted temperature: %.2f\n", finalTemp);
+
+    scanner.close();
+    }
+}
