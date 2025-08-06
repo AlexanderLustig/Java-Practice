27d5ac5069cc11e4c43d5a95335368353d9cfb9b
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-06 11:23:24 -0400

    Create Main20.java

---
+++ Main20.java
@@ -0,0 +1,11 @@
+// day 20 (8/06/2025) -- Again nothing too exciting today. Have an invisalign appointment later but thats about it. 
+
+public class Main20 {
+    public static void main(String[] args) {
+
+      //&& = AND
+      //|| = OR
+      //! = NOT
+
+    }
+}
