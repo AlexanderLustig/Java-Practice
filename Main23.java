99abed6ccede94194aa58bd9ca93097112ce7f19
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-11 11:15:20 -0400

    Create Main23.java

---
+++ Main23.java
@@ -0,0 +1,13 @@
+// day 23 (8/11/2025) -- OK, so got the bot to work. Rn it hasn't made any trades but I think thats cause it hasn't detected any good openings. On another note, John Jay had
+// orientation today and yesterday, Jenan seemed like she wanted to go with me and asked if i was going and what not, but I couldn't cause registration had closed. She then 
+// didnt even try to find another day to go or anything. Idk man like she seems into me, but just doesn't really show it or try that hard I feel.  
+
+public class Main23 {
+    public static void main(String[] args) {
+
+    for(int i = 0; i < 10; i++){
+        System.out.println(i);
+    }
+
+    }
+} 
