8caaf3891c64e8e02809cf8c6d512dbf3b86be54
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-12 09:18:10 -0400

    Create Main24.java

---
+++ Main24.java
@@ -0,0 +1,18 @@
+// day 24 (8/12/2025) -- OK so turns out htere werew some issues legally with how the bot worked, so i had to switch it to a swing trader. Now lets hope everything works
+// perfectly. Ohter then that, I am like 99% sure imma end it with Jenan. Shame mainly cause I invested so much money and bro even got her birthday gifts like tf girl. 
+
+public class Main24 {
+    public static void main(String[] args) {
+
+        //break = end entire loop
+        //continue = skip that current loop
+
+    for(int i = 0; i < 10; i++){
+        if(i == 5){
+            continue;
+        }
+        System.out.println(i);
+    }
+
+    }
+} 
