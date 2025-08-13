9e9da2017337af269c4971eab3e7136b0538ca7b
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-13 15:03:28 -0400

    Create Main25.java

---
+++ Main25.java
@@ -0,0 +1,20 @@
+// day 25 (8/13/2025) -- OK, so more work on the bot. Now im not gonna touch it till Friday to just see if it does work. Also, tried dropshipping yesterday and Print on Demand
+// for some dumb reason but whatever it didnt work and I gave up after a few hours on both. Imma just focus on school and hopefully this bot. Oh also jenan finally texted me
+// first for once after leavin gme on delivered for like a day and a half. So like idk man like I like her alot but these texting patterns bro ughhh. 
+
+public class Main25 {
+    public static void main(String[] args) {
+
+    int counter = 1;
+
+    for(int i = 0; i < 5; i++){
+       for(int j = counter; j > 0; j--){
+            System.out.print("*");
+            
+        }
+        counter++;
+        System.out.print("\n");
+    }
+
+    }
+} 
