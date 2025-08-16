43fd1535cb038183fd9ecf581b9cc1c8841fc1c4
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-16 12:35:10 -0400

    Create Main27.java

---
+++ Main27.java
@@ -0,0 +1,16 @@
+// day 27 (8/16/2025) -- Nothing too new today. I'm trying draftkings with chatGPT so ig we gonna see if that profitable. Tried it once with horse racing and it didn't so 
+// imma try with this. If it works, great. If not, well whatever.
+
+public class Main27 {
+    public static void main(String[] args) {
+
+        System.out.println(add(1, 2, 3));
+
+    }
+    static double add(double a, double b){
+        return(a+b);
+    }
+    static double add(double a, double b, double c){
+        return(a+b+c);
+    }
+} 
