52c6ff14e5da33ee5c7c420099b75a4fbccb0641
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-18 13:24:47 -0400

    Create Main28.java

---
+++ Main28.java
@@ -0,0 +1,19 @@
+// day 28 (8/18/2025) -- So I created a discord bot that will just track the market and I place the trades cause that seemed to be the biggest issue with the bot is it 
+// placing the trades cause idk. So hopefuly that works. Also accidently subscribed to a crypto autotrader for a month, but they offer a refund so after a few day imma see how
+// it does and determine that. 
+
+public class Main28 {
+
+    static int x = 3;
+    public static void main(String[] args) {
+
+    int x = 1;
+
+    doSomething();
+
+    }
+    static void doSomething(){
+        int x = 2;
+        System.out.println(x);
+    }
+} 
