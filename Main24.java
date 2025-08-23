2d7203c78e85486678b5c24531213a87d8064434
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-23 09:49:42 -0400

    Update Main24.java

--- Main24.java
+++ Main24.java
@@ -1,5 +1,4 @@
-// day 24 (8/12/2025) -- OK so turns out htere werew some issues legally with how the bot worked, so i had to switch it to a swing trader. Now lets hope everything works
-// perfectly. Ohter then that, I am like 99% sure imma end it with Jenan. Shame mainly cause I invested so much money and bro even got her birthday gifts like tf girl. 
+// day 24 (8/12/2025) 
 
 public class Main24 {
     public static void main(String[] args) {
