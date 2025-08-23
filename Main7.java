8522be3eafb0fe2e151e7ce85deb1ea514e62977
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-23 09:38:33 -0400

    Update Main7.java

--- Main7.java
+++ Main7.java
@@ -1,8 +1,4 @@
-//day 7(7/14/2025) -- Okok veryyy busy psat couple of days. Got back from a vacation to PA. Got invisaligns - which I hate the feeling of cause like they feel like grills 
-//on my teeth. Then i had back to back dates. Took Sierra out first. and like she pretty and we vibed, but like ehhh 5/10. Then i had a date with Jenan, which i enjoyed a
-//bunch more. Like she is tall and pretty and a NERD. So rn she easily top choice and I hope it works out with her. Oh and i also started doing these jornal things cause like
-// I feel like future me would enjoy reading what i thought on these days. So imma just be writing a bunch of weird toughts here.
-
+//Day 7(7/14/2025)
 import java.util.Scanner;
 
 public class Main7 {
