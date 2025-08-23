0bde90a9948c56658954129f4d831c591df20994
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-23 09:51:35 -0400

    Update Main27.java

--- Main27.java
+++ Main27.java
@@ -1,5 +1,4 @@
-// day 27 (8/16/2025) -- Nothing too new today. I'm trying draftkings with chatGPT so ig we gonna see if that profitable. Tried it once with horse racing and it didn't so 
-// imma try with this. If it works, great. If not, well whatever.
+// day 27 (8/16/2025)
 
 public class Main27 {
     public static void main(String[] args) {
