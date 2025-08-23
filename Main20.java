56fbd4a7929ffaeed764352707c5ee7706b755fb
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-23 09:47:28 -0400

    Update Main20.java

--- Main20.java
+++ Main20.java
@@ -1,4 +1,4 @@
-// day 20 (8/06/2025) -- Again nothing too exciting today. Have an invisalign appointment later but thats about it. 
+// day 20 (8/06/2025) 
 
 public class Main20 {
     public static void main(String[] args) {
