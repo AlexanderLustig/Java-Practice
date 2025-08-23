31b02fda9b0ddd021a5d2c1632094fd465587a56
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-23 09:42:12 -0400

    Update Main12.java

--- Main12.java
+++ Main12.java
@@ -1,7 +1,4 @@
-// day 12 (7/23/2025) -- Had a 3rd date with Jenan. Went SUPPERRR amazing. During the movie she was leaning on my shoulder for most of it and like
-//whenever we talked we got really clsoe to eachother and idk that felt nice. Plus we also held hands that day also. But my original plan to hold her hand was
-//ruined cause a dumbass tree delayed the train. But it still worked out in the end. Now i wanna try to kiss her on the 4th date. Plus i need to give her her
-//presents so we will see how that goes next.
+// day 12 (7/23/2025) 
 
 public class Main12 {
     public static void main(String[] args) {
