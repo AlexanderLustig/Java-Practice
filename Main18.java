549a66dd8697072c0fa8a2c65e48c727f8555979
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-23 09:46:39 -0400

    Update Main18.java

--- Main18.java
+++ Main18.java
@@ -1,9 +1,4 @@
-// day 18 (8/04/2025) -- Ok, so had a date then work so im back now. Date went ok, but in the end it sounded like she freindzoned me.
-// so i asked what she meant when i got home and she said she open to dating in near future which is a good sign. Plus she mentioned
-// that if someone were to ask bout me she would just say im her bf which ig is a really good sign. Now just doing research in how to 
-// make a gift basket for her to ask her out officially. Also, rn she has all green flags except for her texting. Bro it takes her
-// hours and hours to respond back like ik she mentioned how this is normal for her and she does it to her friends and family also 
-// but like still ughhh its something imma have to really get used to. 
+// day 18 (8/04/2025) 
 
 import java.util.Scanner;
 
