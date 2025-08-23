aeb28d5931ff9eb03dc6b133a9682927e13f9893
Alexander Lustig <94746890+AlexanderLustig@users.noreply.github.com>
2025-08-23 09:51:25 -0400

    Update Main26.java

--- Main26.java
+++ Main26.java
@@ -1,6 +1,4 @@
-// day 26 (8/15/2025) -- OK, so I talked things out with Jenan. We are still together, and we got alot out and I'm content now. Now we just see how life goes. Like she 
-// even said how she doesn't wanna lose me which is AMAZING. But ya, also decided to completely remake the trading bot so hopefully this new one actually works now. 
-// Also gonna try fantasy betting with chatGPT. So lets see if that works also???
+// day 26 (8/15/2025) 
 
 public class Main26 {
     public static void main(String[] args) {
